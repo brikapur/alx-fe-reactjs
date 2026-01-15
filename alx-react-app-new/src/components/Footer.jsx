39d7c9f8a824c9ac5bca486bbae7fbb4f5335ba4
Bridget <bridgetnduta12@gmail.com>
2026-01-15 18:52:21 +0300

    Apply inline CSS styling to React components

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer () {
+    return(
+        <footer style={{ background: 'black'}}>
+            <p style={{color: 'white', fontSize: '20px',textAlign: 'center'}}>© 2023 City Lovers</p>
+        </footer>
+    );
+}
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,8 @@
+function MainContent () {
+    return(
+        <main style={{padding: '10px', margin: '10px',background: '#fc5555',minHeight: '100px'}}>
+            <p style={{fontSize: '30px', fontWeight: 'bold', color: '#222'}}>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );    
+}
+export default MainContent;
