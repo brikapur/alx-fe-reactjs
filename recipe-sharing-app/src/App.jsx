3431cb28f13468188d26f321884e33bdcb2b988d
Bridget <bridgetnduta12@gmail.com>
2026-01-25 15:46:16 +0300

    Rename AppRecipeForm to AddRecipeForm and fix imports

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,22 +1,14 @@
-import { useState } from 'react'
-import './App.css'
-import RecipeList from './components/RecipeList'
-import AppRecipeForm from './components/AppRecipeForm'
+import './App.css';
+import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
 
 function App() {
-  const [recipes, setRecipes] = useState ([]);
-
-const addRecipe = (newRecipe) => {
-  setRecipes([...recipes,newRecipe]);
-};
   return (
-    <div className ="app">
+    <div className="app">
       <h1>My Recipe App</h1>
-      {/* Form to add recipes */}
-      <AppRecipeForm onAddRecipe = {addRecipe} />
-      <RecipeList recipes ={recipes} />
+      <AddRecipeForm />
+      <RecipeList />
     </div>
-    
   );
 }
 
