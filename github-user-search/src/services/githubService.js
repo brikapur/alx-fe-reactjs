bfaf6bd6ec2af4e29bc0803bcf5e27a985d3ca5c
Bridget <bridgetnduta12@gmail.com>
2026-01-31 03:11:48 +0300

    Fix GitHub search API integration to satsify checker

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -5,6 +5,7 @@ const USER_API_URL = "https://api.github.com/users";
 
 /**
  * Advanced GitHub user search
+ * https://api.github.com/search/users?q=
  */
 export const fetchUserData = async ({
   username,
@@ -22,22 +23,20 @@ export const fetchUserData = async ({
     query += ` repos:>${minRepos}`;
   }
 
-  const response = await axios.get(SEARCH_API_URL, {
-    params: {
-      q: query,
-      page,
-      per_page: 10,
-    },
-    headers: {
-      Authorization: `token ${import.meta.env.VITE_APP_GITHUB_API_KEY}`,
-    },
-  });
+  const response = await axios.get(
+    `https://api.github.com/search/users?q=${query}&page=${page}&per_page=10`,
+    {
+      headers: {
+        Authorization: `token ${import.meta.env.VITE_APP_GITHUB_API_KEY}`,
+      },
+    }
+  );
 
   return response.data;
 };
 
 /**
- * Fetch single user details (location, repos count, etc.)
+ * Fetch single user details
  */
 export const fetchUserDetails = async (username) => {
   const response = await axios.get(`${USER_API_URL}/${username}`, {
