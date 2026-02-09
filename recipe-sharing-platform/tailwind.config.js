c14c9f9df8b5e78992a16ea363f5cd49e64db7d9
Bridget <bridgetnduta12@gmail.com>
2026-02-09 23:17:18 +0300

    Add the changes made in tailwind.config.js file

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -3,9 +3,11 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
   ],
+  darkMode: false, // You can set it to 'media' or 'class' if needed
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
