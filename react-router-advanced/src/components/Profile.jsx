6019357b77196b9015f024966a1a0ad695088780
Bridget <bridgetnduta12@gmail.com>
2026-02-22 23:34:05 +0300

    Move ProtectedRoute to components folder and fix structure

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -2,7 +2,7 @@ import { Routes, Route, Link, Outlet } from "react-router-dom";
 
 import ProfileDetails from "../pages/ProfileDetails";
 import ProfileSettings from "../pages/ProfileSettings";
-import ProtectedRoute from "../routes/ProtectedRoute";
+import ProtectedRoute from "./ProtectedRoute";
 
 const Profile = () => {
   return (
